57c485abe6fef94f0d1318d370a7944a6e6e0fd5
Patrice Ferlet <metal3d@gmail.com>
2019-06-18 23:30:34 +0200

    Add some routes and explain how to call them

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ source v/bin/activate
 pip3 install -r requirements.txt
 FLASK_APP=main:app FLASK_DEBUG=1 flask run
 ```
+
+Install "httpie" package from your Unix(Like) distribution.
+
+Then in another terminal:
+```bash
+$ http :5000
+$ http :5000/greeting/Jon
+
+# test json {"name": "Jon"}
+$ http :5000/echo name=Jon
+
+# test with fogotten "name" key should give an error
+$ http :5000/echo foo=bar
+```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 
@@ -6,3 +7,18 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return "Hello !!"
+
+
+@app.route('/greeting/<user>')
+def greeting(user):
+    return "Hello dear %s" % user
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    if 'name' not in request.json:
+        return "Please set name in json request", 500
+
+    return """
+    You gave {name} as "name" value
+    """.format(**request.json)
